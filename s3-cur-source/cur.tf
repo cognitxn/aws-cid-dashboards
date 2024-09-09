994247d77b5c6a5ff68af20f30c27e11d9ca8639
osibanda <oscarmapfumo.sibanda@teampicnic.com>
2024-09-10 00:18:33 +0200

    78209427 - Add tags to cur_report_definition

--- s3-cur-source/cur.tf
+++ s3-cur-source/cur.tf
@@ -6,15 +6,21 @@ resource "aws_cur_report_definition" "source" {
     aws_s3_bucket_policy.source
   ]
 
-  report_name                = "${var.resource_prefix}-${var.cur_name_suffix}"
-  time_unit                  = "HOURLY"
-  format                     = "Parquet"
-  compression                = "Parquet"
+  additional_artifacts       = ["ATHENA"]
   additional_schema_elements = var.enable_split_cost_allocation_data ? ["RESOURCES", "SPLIT_COST_ALLOCATION_DATA"] : ["RESOURCES"]
+  compression                = "Parquet"
+  format                     = "Parquet"
+  refresh_closed_reports     = true
+  report_name                = "${var.resource_prefix}-${var.cur_name_suffix}"
+  report_versioning          = "OVERWRITE_REPORT"
+  s3_prefix                  = "cur/${local.account_id}"
   s3_bucket                  = aws_s3_bucket.source.bucket
   s3_region                  = local.aws_region
-  s3_prefix                  = "cur/${local.account_id}"
-  additional_artifacts       = ["ATHENA"]
-  report_versioning          = "OVERWRITE_REPORT"
-  refresh_closed_reports     = true
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.resource_prefix}-${var.cur_name_suffix}"
+    }
+  )
+  time_unit = "HOURLY"
 }
